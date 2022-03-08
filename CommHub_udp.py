31f139cbbea927b1f3ea7d98d0f672c2e8c1b954
AidenNeale <aidenneale@sky.com>
2022-03-08 16:23:07 +0000

    Removed unused functionality from file and added instantiation for Tracking

--- CommHub_udp.py
+++ CommHub_udp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 import time
+
 from threading import Lock
 
-from commhub import *
+from ArUcoTracker import ArUcoTracker
+from commhub import CommHub
 
 # Parameters for the Buzz ComHub
 FORWARD_FREQ = 50  # Hz
@@ -11,14 +12,11 @@ FORWARD_FREQ = 50  # Hz
 CAM_IP = '127.0.0.1'
 HUB_IP = '127.0.0.1'
 
-lock_opti = Lock()
-
-
 
 if __name__ == '__main__':
 
   comm_hub = CommHub(forward_freq=FORWARD_FREQ)
-
+  robotTracker = ArUcoTracker()
   try:
     while True:
       time.sleep(0.1)
