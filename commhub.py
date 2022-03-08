a813ad24290cbc2ae81502dd90027501597b68ef
AidenNeale <aidenneale@sky.com>
2022-03-08 18:38:10 +0000

    Improved readability of the code by clarifying variable names

--- commhub.py
+++ commhub.py
@@ -1,16 +1,11 @@
-from threading import Thread, Lock
-import socket
-import struct
-import time
 import numpy as np
-from os import urandom
+import socket
+import time
+
 from collections import defaultdict
+from threading import Thread, Lock
 
 from packet import Packet
-
-
-def wrap_angle(angle):
-    return ((angle + np.pi) % (2 * np.pi) - np.pi)
 
 
 class CommHub:
@@ -35,11 +30,12 @@ class CommHub:
         self.packets_lock = Lock()
         self.id2ip = {}
 
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         try:
-            self.s.bind((host, port))
+            self.socket.bind((host, port))
         except OSError as e:
-            self.s.close()
+            self.socket.close()
             print("ERROR: Trying to create a CommHub on a busy address")
             raise e
 
@@ -65,15 +61,15 @@ class CommHub:
     def receive(self):
         print("Receiving Thread Initialised...")  # Debug
         while self.alive: # While Comm Hub Alive
-            p = Packet.from_socket(self.s) # Read Packet from Socket
-            print(p)
+            received_packet = Packet.from_socket(self.socket) # Read Packet from Socket
+            print(received_packet)
             # Update IP/id database
-            self.id2ip[p.comm_id] = p.addr
+            self.id2ip[received_packet.comm_id] = received_packet.addr
 
-            if p:
-                print("Received packet from Robot {}".format(p.comm_id))  # Debug
+            if received_packet:
+                print("Received packet from Robot {}".format(received_packet.comm_id))  # Debug
                 self.packets_lock.acquire()
-                self.packets[p.comm_id].append(p)
+                self.packets[received_packet.comm_id].append(received_packet)
                 self.packets_lock.release()
             else:
                 break
@@ -97,27 +93,26 @@ class CommHub:
     '''
     def forward_packets(self):
         # For all known robots, get addresses and ids
-        for id1, addr1 in self.id2ip.items():
+        for robot_id1, robot_addr1 in self.id2ip.items():
 
             # If there are packets from these robots, put them into a data structure
             self.packets_lock.acquire()
-            tmppackets = self.packets[id1][:]
-            self.packets[id1] = []
+            tmppackets = self.packets[robot_id1][:]
+            self.packets[robot_id1] = []
             self.packets_lock.release()
             if len(tmppackets) == 0:
-                tmppackets = [Packet(0.0, 0.0, 0.0, id1)]
-            # print("Reaching Forward_Packets")
+                tmppackets = [Packet(0.0, 0.0, 0.0, robot_id1)]
             # Cycle through all other robots and forward the packets
-            for id2, addr2 in self.id2ip.items():
+            for robot_id2, addr2 in self.id2ip.items():
                 try:
                     # Compute relative vector and distance
-                    rel_vector = self.locs[id1][:-1] - self.locs[id2][:-1]
+                    rel_vector = self.locs[robot_id1][:-1] - self.locs[robot_id2][:-1]
                     distance = np.linalg.norm(rel_vector)
 
                     # Send updated own location to the robot
-                    if id1 == id2:
-                        self.send_to(id2, Packet(
-                            self.locs[id1][0], self.locs[id1][1], self.locs[id1][2], id1, theta=self.locs[id1][3]), id1)
+                    if robot_id1 == robot_id2:
+                        self.send_to(robot_id2, Packet(
+                            self.locs[robot_id1][0], self.locs[robot_id1][1], self.locs[robot_id1][2], robot_id1, theta=self.locs[robot_id1][3]), robot_id1)
 
                     # Only forward packets if within comms distance, in RAB format
                     else:  # if distance < self.neighbor_distance:
@@ -127,15 +122,15 @@ class CommHub:
                             rel_vector[2], np.linalg.norm(rel_vector[:2]))
 
                         # convert angle to receivers coordinate
-                        rel_theta = rel_theta - self.locs[id2][3]
+                        rel_theta = rel_theta - self.locs[robot_id2][3]
                         # Wrap angles
-                        rel_theta = rel_theta + 2*np.pi if rel_theta < 0. else rel_theta
-                        rel_phi = rel_phi + 2*np.pi if rel_phi < 0. else rel_phi
+                        azimuth = rel_theta + 2*np.pi if rel_theta < 0. else rel_theta
+                        elevation = rel_phi + 2*np.pi if rel_phi < 0. else rel_phi
 
-                        self.send_to_with_rb(id2, tmppackets, np.array(
-                            (distance*100.0, rel_theta, rel_phi)))  # *100.0 to obtain [cm] on board
+                        self.send_to_with_rb(robot_id2, tmppackets, np.array(
+                            (distance*100.0, azimuth, elevation)))  # *100.0 to obtain [cm] on board
                 except KeyError as e:
-                    pass  # print("No locs for Robot {}".format(id2))
+                    pass  # print("No locs for Robot {}".format(robot_id2))
 
 
     '''
@@ -148,13 +143,13 @@ class CommHub:
         try:
             packets[0]
         except (AttributeError, TypeError):
-            self.s.sendto(packets.byte_string(), self.id2ip[destination])
+            self.socket.sendto(packets.byte_string(), self.id2ip[destination])
             print(" comm packet sender {} receiver {}".format(sender,destination))
             return
-        # m = bytes()
-        for p in packets:
-            m = p.byte_string()
-            self.s.sendto(m, self.id2ip[destination])
+
+        for packet in packets:
+            msg = packet.byte_string()
+            self.socket.sendto(msg, self.id2ip[destination])
 
 
     '''
@@ -163,20 +158,18 @@ class CommHub:
     :param destination: the id of the robot to send the packages to
     :param packets: a list of Packet objects, or just a single Packet
     '''
-
     def send_to_with_rb(self, destination, packets, rel_rb):
         try:
             packets[0]
         except (AttributeError, TypeError):
             packets.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
-            self.s.sendto(packets.byte_string(), self.id2ip[destination])
+            self.socket.sendto(packets.byte_string(), self.id2ip[destination])
             return
-        # m = bytes()
-        for p in packets:
-            p.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
-            m = p.byte_string()
-            # print("Time take to send {} sender {} receiver {}".format(time.time()-p.received_time,sender,receiver))
-            self.s.sendto(m, self.id2ip[destination])
+
+        for packet in packets:
+            packet.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
+            msg = packet.byte_string()
+            self.socket.sendto(msg, self.id2ip[destination])
 
 
 
