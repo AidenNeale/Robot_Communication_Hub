ffe55ca01289a7a4272e7fe4a56ebbf207698f40
AidenNeale <aidenneale@sky.com>
2022-03-10 16:40:26 +0000

    Added more instantiation options

--- CommHub_udp.py
+++ CommHub_udp.py
@@ -11,12 +11,12 @@ FORWARD_FREQ = 50  # Hz
 
 # This is the IP address in which the Camera and Communication Hub are hosted
 SERVER_IP = '144.32.175.138'
-
+PORT = 4242
 
 if __name__ == '__main__':
 
-  comm_hub = CommHub(forward_freq=FORWARD_FREQ, host=SERVER_IP)
-  # robotTracker = ArUcoTracker()
+  comm_hub = CommHub(forward_freq=FORWARD_FREQ, host=SERVER_IP, port=PORT)
+  robotTracker = ArUcoTracker(HOST=SERVER_IP, PORT=PORT)
   try:
     while True:
       time.sleep(0.1)
