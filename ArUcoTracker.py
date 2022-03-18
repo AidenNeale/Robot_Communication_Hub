789ad41c74b559d827916643cf37b71945c9459f
AidenNeale <aidenneale@sky.com>
2022-03-18 17:26:57 +0000

    Well Documented Functionality and Added Theta Calculation

--- ArUcoTracker.py
+++ ArUcoTracker.py
@@ -1,6 +1,7 @@
 from threading import Thread, Lock
 
 import numpy as np
+import math
 import sys
 import time
 
@@ -40,7 +41,7 @@ class ArUcoTracker:
     "352x288":   [352, 288],
     "640x480":   [640, 480],
     "1024x768":  [1024, 768],
-    "1280x720": [1280, 720]
+    "1280x720":  [1280, 720]
   }
 
   def __init__(self, arUco_type="DICT_5X5_50", HOST = '127.0.0.1', PORT = '4242',
@@ -76,18 +77,23 @@ class ArUcoTracker:
 
   def init_camera(self) -> cv2.VideoCapture:
     '''
+    Initialise the Camera Setup to enable Video Capture
 
+    Returns:
+    --------
+    cap -> cv2.VideoCapture
+      CV2's VideoCapture instance
     '''
-    # Open Video Capture with Chosen Camera on appropriate USB
+    # Open Video Capture with External Chosen Camera on appropriate USB
     cap = cv2.VideoCapture(self.CHOSEN_CAMERA)
     if not cap.isOpened():
-      # Fallback attempt to open Video Capture with Laptop Webcam
+      # Fallback attempt to open Video Capture with onboard Webcam
       cap = cv2.VideoCapture(0)
     if not cap.isOpened():
       print("Cannot Open Camera")
       exit()
 
-    # Change Resolution of the frame to 1024X768
+    # Change Resolution of the frame to 1280x720
     self.change_res(cap, self.POSSIBLE_RESOLUTIONS["1280x720"])
     time.sleep(1.5)
     return cap
@@ -96,7 +102,14 @@ class ArUcoTracker:
 
   def change_res(self, cap, resolution) -> None:
     '''
+    Change the resolution of the Video Capture
 
+    Parameters:
+    -----------
+    cap -> Instance of CV2's Video Capture
+      ...
+    resolution -> 2-Element List of resolution settings: X x Y
+      ...
     '''
     cap.set(3, resolution[0])
     cap.set(4, resolution[1])
@@ -105,8 +118,14 @@ class ArUcoTracker:
 
   def calculate_scale(self) -> float:
     '''
+    Automates the calculation the pixel scale factor for a real world metre
 
+    Returns:
+    --------
+    oneMetreScaleFactor -> float
+      This value is used to scale coordinates in tracking
     '''
+
     oneMetreScaleFactor = 0
 
     while True:
@@ -136,7 +155,6 @@ class ArUcoTracker:
 
         # At this stage, position_markers will have stored the corners of the
         # ArUco Tags
-        # print(f"{positionMarkers}") # Debug
         topMaxX, topMaxY = float('-inf'), float('-inf')
         bottomMinX, bottomMinY = float('inf'), float('inf')
 
@@ -153,7 +171,7 @@ class ArUcoTracker:
           bottomMinX = min(positionMarkers[indexes]['bottomLeft'][0], bottomMinX)
           bottomMinY = min(positionMarkers[indexes]['topLeft'][1], bottomMinY)
           bottomMinY = min(positionMarkers[indexes]['topRight'][1], bottomMinY)
-        print(f"Highest Coordinate: {topMaxX}: {topMaxY}, Lowest Coordinate: {bottomMinX}: {bottomMinY}") # Debug
+        # print(f"Highest Coordinate: {topMaxX}: {topMaxY}, Lowest Coordinate: {bottomMinX}: {bottomMinY}") # Debug
 
         # Set global values to keep track of arena Coordinates
         self.arenaMaxX, self.arenaMaxY = topMaxX, topMaxY
@@ -193,20 +211,26 @@ class ArUcoTracker:
       if len(tags) > 0:
         ids = ids.flatten()
         # loop over the detected ArUCo corners
-        positionMarkers = {}
         for (markerCorner, markerID) in zip(tags, ids):
           # Ignores ArUco Tags of ID 'POSITION_MARKERS' as these are reserved for trackers
-          # if markerID == self.POSITION_MARKERS:
-            # pass
+          if markerID == self.POSITION_MARKERS:
+            continue
           corners = markerCorner.reshape((4, 2))
           (topLeft, topRight, bottomRight, bottomLeft) = corners
 
+          frontOfTagX = (topLeft[0] + topRight[0]) / 2
+          frontOfTagY = (topLeft[1] + topRight[1]) / 2
+
           # Finds the Centre Point of the robot and draws onto the frame
-          centreX, centreY = self.get_robot_centre(topLeft, bottomRight)
+          centreX, centreY, centreZ = self.get_robot_centre(topLeft, bottomRight)
           cv2.circle(frame, (int(centreX), int(centreY)), 4, (0, 0, 255), -1)
 
+          bearing = math.atan2(frontOfTagY - centreY, frontOfTagX - centreX)
+
           # Scales the Robots Centre Point to Metres
-          coordinates = self.scale_coordinates(centreX, centreY)
+          coordinates = self.scale_coordinates(centreX, centreY, centreZ)
+
+          coordinates.append(bearing)
           # print(f"Robot {markerID} is positioned: {coordinates}")
           self.packets_lock.acquire()
           self.id2coords[markerID] = coordinates
@@ -220,36 +244,53 @@ class ArUcoTracker:
 
 
 
-  def scale_coordinates(self, x, y):
+  def scale_coordinates(self, x, y, z):
     '''
-    Scales any coordinates to convert pixels to metres
+    Scales Pixel Coordinates to real world values in metres
+
+    Parameters:
+    -----------
+    x -> int
+      Pixel coordinate for the X-Axis
+    y -> int
+      Pixel Coordinate for the Y-Axis
+
+    Returns:
+    Coordinates -> list
+      Returns a list of scaled coordinates with a Z-Axis assumed on a flat plane
     '''
+
     scaledX = (x - self.arenaMinX) / self.scale
     scaledY = (y - self.arenaMinY) / self.scale
-    scaledZ = 0
-    return (scaledX, scaledY, scaledZ)
+    scaledZ = z
+    return [scaledX, scaledY, scaledZ]
 
 
 
   def get_robot_centre(self, topLeft, bottomRight):
     '''
     Calculates the centre of the robot by taking the averages of the Tag Corners
+
+    Returns:
+    --------
+    Coordinates -> tuple
+      The centre positionings of the ArUco Tags and assumes robot is on a flat plane
     '''
-    centreX = (abs(topLeft[0]+bottomRight[0])/2)
-    centreY = (abs(topLeft[1]+bottomRight[1])/2)
-    return (centreX, centreY)
+    centreX = (topLeft[0]+bottomRight[0])/2
+    centreY = (topLeft[1]+bottomRight[1])/2
+    centreZ = 0
+    return (centreX, centreY, centreZ)
 
 
-
-  '''
-
-  '''
   def send_coordinates(self):
+    '''
+    Updates the positions of the robot of ID: robotID with updated scaled coordinates
+    '''
     while True:
       readDictionary = self.id2coords.items()
       try:
         for robotID, robotCoordinates in readDictionary:
-          self.CommHub.update_position(robotID, robotCoordinates, 0)
+          self.CommHub.update_position(robotID, robotCoordinates[:3], robotCoordinates[-1:])
       except RuntimeError:
         pass
 
