22c4924f40ce41e55e9d22d5ac1709a91768d1dd
AidenNeale <aidenneale@sky.com>
2022-03-18 18:17:03 +0000

    Added Documentation for the CommHub

--- commhub.py
+++ commhub.py
@@ -53,136 +53,152 @@ class CommHub:
         self.received_thread.start()
 
 
-    '''
-    PRIVATE
-    New thread that blocks until new packet comes. Packets are added to self.packets
-    '''
-
     def receive(self):
-        print("Receiving Thread Initialised...")  # Debug
-        while self.alive: # While Comm Hub Alive
-            received_packet = Packet.from_socket(self.socket) # Read Packet from Socket
-            # Update IP/id database
-            self.id2ip[received_packet.comm_id] = received_packet.addr
+      '''
+      New Thread that blocks until a new packet arrives on the socket. Packets
+      are added to self.packets and IPs of senders stored according to their ID
+      '''
 
-            if received_packet:
-                # print("Received packet from Robot {}".format(received_packet.comm_id))  # Debug
-                self.packets_lock.acquire()
-                self.packets[received_packet.comm_id].append(received_packet)
-                self.packets_lock.release()
-            else:
-                break
+      print("Receiving Thread Initialised...")  # Debug
+      while self.alive: # While Comm Hub Alive
+        received_packet = Packet.from_socket(self.socket) # Read Packet from Socket
+        # Update IP/id database
+        self.id2ip[received_packet.comm_id] = received_packet.addr
+
+        if received_packet:
+          # print("Received packet from Robot {}".format(received_packet.comm_id))  # Debug
+          self.packets_lock.acquire()
+          self.packets[received_packet.comm_id].append(received_packet)
+          self.packets_lock.release()
+        else:
+          break
 
 
-    '''
-    PRIVATE
-    Automatically call CommHub.forward_packets at a certain frequency
-    :param period: float. Time between calls to CommHub.forward_packets in seconds
-    '''
     def auto_forward(self, period):
+      '''
+      New thread to automatically call CommHub.forward_packets at a certain frequency
+
+      Parameters:
+      -----------
+      period -> int
+        Time Delay between handling of packet forwarding
+      '''
       print("Forwarding Thread Initialised...")
       while self.alive:
         time.sleep(period)
         self.forward_packets()
 
 
-    '''
-    Keep the communication flowing between robots.
-    All information shared between robots, and any updates to positions
-    are not sent unless this function is called.
-    '''
     def forward_packets(self):
-        # For all known robots, get addresses and ids
-        for robot_id1, robot_addr1 in self.id2ip.items():
+      '''
+      Drives communication between robots. All information shared between robots, and any
+      updates to positions are not sent unless this function is called
+      '''
+      # For all known robots, get addresses and ids
+      for robot_id1, robot_addr1 in self.id2ip.items():
 
-            # If there are packets from these robots, put them into a data structure
-            self.packets_lock.acquire()
-            tmppackets = self.packets[robot_id1][:]
-            self.packets[robot_id1] = []
-            self.packets_lock.release()
-            if len(tmppackets) == 0:
-              tmppackets = [Packet(0.0, 0.0, 0.0, robot_id1)]
+        # If there are packets from these robots, put them into a data structure
+        self.packets_lock.acquire()
+        tmppackets = self.packets[robot_id1][:]
+        self.packets[robot_id1] = []
+        self.packets_lock.release()
+        if len(tmppackets) == 0:
+          tmppackets = [Packet(0.0, 0.0, 0.0, robot_id1)]
 
-            # Cycle through all other robots and forward the packets
-            for robot_id2, addr2 in self.id2ip.items():
-                try:
-                    # Compute relative vector and distance
-                    rel_vector = self.locs[robot_id1][:-1] - self.locs[robot_id2][:-1]
-                    distance = np.linalg.norm(rel_vector)
+        # Cycle through all other robots and forward the packets
+        for robot_id2, addr2 in self.id2ip.items():
+          try:
+            # Compute relative vector and distance
+            rel_vector = self.locs[robot_id1][:-1] - self.locs[robot_id2][:-1]
+            distance = np.linalg.norm(rel_vector)
 
-                    # Send updated own location to the robot
-                    if robot_id1 == robot_id2:
-                        self.send_to(robot_id2, Packet(
-                            self.locs[robot_id1][0], self.locs[robot_id1][1], self.locs[robot_id1][2], robot_id1, theta=self.locs[robot_id1][3]))
+            # Send updated own location to the robot
+            if robot_id1 == robot_id2:
+              self.send_to(robot_id2, Packet(
+                self.locs[robot_id1][0], self.locs[robot_id1][1], self.locs[robot_id1][2], robot_id1, theta=self.locs[robot_id1][3]))
 
-                    # Only forward packets if within comms distance, in RAB format
-                    else:  # if distance < self.neighbor_distance:
-                        # Compute azimuth (theta) and elevation (phi)
-                        rel_theta = np.arctan2(rel_vector[1], rel_vector[0])
-                        rel_phi = np.arctan2(
-                            rel_vector[2], np.linalg.norm(rel_vector[:2]))
+            # Only forward packets if within comms distance, in RAB format
+            else:  # if distance < self.neighbor_distance:
+              # Compute azimuth (theta) and elevation (phi)
+              rel_theta = np.arctan2(rel_vector[1], rel_vector[0])
+              rel_phi = np.arctan2(rel_vector[2], np.linalg.norm(rel_vector[:2]))
 
-                        # convert angle to receivers coordinate
-                        rel_theta = rel_theta - self.locs[robot_id2][3]
-                        # Wrap angles
-                        azimuth = rel_theta + 2*np.pi if rel_theta < 0. else rel_theta
-                        elevation = rel_phi + 2*np.pi if rel_phi < 0. else rel_phi
+              # convert angle to receivers coordinate
+              rel_theta = rel_theta - self.locs[robot_id2][3]
+              # Wrap angles
+              azimuth = rel_theta + 2*np.pi if rel_theta < 0. else rel_theta
+              elevation = rel_phi + 2*np.pi if rel_phi < 0. else rel_phi
 
-                        self.send_to_with_rb(robot_id2, tmppackets, np.array(
-                            (distance*100.0, azimuth, elevation)))  # *100.0 to obtain [cm] on board
-                except KeyError as e:
-                    pass  # print("No locs for Robot {}".format(robot_id2))
+              self.send_to_with_rb(robot_id2, tmppackets, np.array(
+                (distance*100.0, azimuth, elevation)))  # *100.0 to obtain [cm] on board
+          except KeyError as e:
+            pass  # print("No locs for Robot {}".format(robot_id2))
 
 
-    '''
-    PRIVATE
-    Send packets to a destination
-    :param destination: the id of the robot to send the packages to
-    :param packets: a list of Packet objects, or just a single Packet
-    '''
+
     def send_to(self, destination, packets):
-        try:
-            packets[0]
-        except (AttributeError, TypeError):
-            self.socket.sendto(packets.byte_string(), self.id2ip[destination])
-            # print(" comm packet sender {} receiver {}".format(sender,destination))
-            return
+      '''
+      Update a Robots Own Position by sending 'packets' to 'destination'
 
-        for packet in packets:
-            msg = packet.byte_string()
-            self.socket.sendto(msg, self.id2ip[destination])
+      Parameters:
+      -----------
+      destination -> int
+        Destination Robot ID to send Packets to
+      packets -> list of Packet Objects | Single Packet Object
+        Contains information to forward to robots with structure found in packet.py
+      '''
+      try:
+        packets[0]
+      except (AttributeError, TypeError):
+        self.socket.sendto(packets.byte_string(), self.id2ip[destination])
+        # print(" comm packet sender {} receiver {}".format(sender,destination))
+        return
+
+      for packet in packets:
+        msg = packet.byte_string()
+        self.socket.sendto(msg, self.id2ip[destination])
 
 
-    '''
-    PRIVATE
-    Send packets to a destination with range and bearing incorporated
-    :param destination: the id of the robot to send the packages to
-    :param packets: a list of Packet objects, or just a single Packet
-    '''
+
     def send_to_with_rb(self, destination, packets, rel_rb):
-        try:
-            packets[0]
-        except (AttributeError, TypeError):
-            packets.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
-            self.socket.sendto(packets.byte_string(), self.id2ip[destination])
-            return
+      '''
+      Update a Robots Own Position by sending 'packets' to 'destination'
 
-        for packet in packets:
-            packet.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
-            msg = packet.byte_string()
-            self.socket.sendto(msg, self.id2ip[destination])
+      Parameters:
+      -----------
+      destination -> int
+        Destination Robot ID to send Packets to
+      packets -> list of Packet Objects | Single Packet Object
+        Contains information to forward to robots with structure found in packet.py
+      rel_rb -> np.array
+        Array containing the Distance, Range and Bearing between the source and
+        destination robot
+      '''
+      try:
+        packets[0]
+      except (AttributeError, TypeError):
+        packets.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
+        self.socket.sendto(packets.byte_string(), self.id2ip[destination])
+        return
 
+      for packet in packets:
+        packet.set_rb(rel_rb[0], rel_rb[1], rel_rb[2])
+        msg = packet.byte_string()
+        self.socket.sendto(msg, self.id2ip[destination])
 
-
-    # '''
-    # Update the position of the specified robot.
-    # :param robot_id: int. the id of the robot whose position is to be updated
-    # :param loc: list, tuple, or numpy.array. The updated position of the robot
-    # :param yaw float yaw euiler angle
-    # :return: bool. True if update was successful
-    # '''
 
     def update_position(self, robot_id, loc, yaw):
-        self.locs[int(robot_id)] = np.append(np.array(loc), yaw)
-        # print("Robot {} pos {}".format(robot_id, self.locs[robot_id]))
-        return True
+      '''
+      Update the position of the specified robot from information from the Camera
+
+      Parameters:
+      -----------
+      robot_id -> int
+        The ID of the robot whose position is to be updated
+      loc -> list/tuple/numpy.array
+        The updated position of the robot
+      yaw -> float
+        Bearing of the Robot
+      '''
+      self.locs[int(robot_id)] = np.append(np.array(loc), yaw)
+      # print("Robot {} pos {}".format(robot_id, self.locs[robot_id]))
--- packet.py
+++ packet.py
@@ -22,81 +22,107 @@ class Packet:
         self.received_time = received_time
         self.addr = addr
 
+
     def set_rb(self, rng, bearing, elevation):
-        self.x = rng
-        self.y = bearing
-        self.z = elevation
+      '''
+      Setter Function for Range, Bearing and Elevation
 
-    '''
-    PRIVATE
-    Convert packet to a bytes object containing all the information.
+      Parameters:
+      ------------
+      rng ->
+        Range between two robots
+      bearing ->
+        Bearing between two robots
+      elevation ->
+        Elevation between the robot and the camera
+      '''
+      self.x = rng
+      self.y = bearing
+      self.z = elevation
 
-    Contents:
-        2 bytes comm_id
-        4 bytes x
-        4 bytes y
-        4 bytes z
-        4 bytes theta
-        for each message {
-            2 bytes message length (n)
-            n bytes message
-        }
-        4 bytes (0000)
-
-    :return b_string: bytes object representing entire packet
-    '''
 
     def byte_string(self):
-        b_string = struct.pack('=H4f', int(self.comm_id), float(
-            self.x), float(self.y), float(self.z), float(self.theta))
-        for msg in self.msgs:
-            b_string += struct.pack('H', len(msg))
-            b_string += msg
-        b_string += struct.pack('I', 0)
-        while len(b_string) < MSG_SIZE:
-            b_string += struct.pack('B', 0)
-        return b_string
+      '''
+      Convert packet to a bytes object containing all the information.
 
-    '''
-    PRIVATE
-    Create a packet from a Kh4 socket
-    Block until a string of bytes comes in, and unpack these bytes into a new Packet object.
-    The incoming bytes are in the form described in the documentation for Packet.byte_string
-    :param s: socket object
-    :return: Packet object or False if any socket.error occured
-    '''
+      Contents:
+      ---------
+      * 2 bytes comm_id
+      * 4 bytes x
+      * 4 bytes y
+      * 4 bytes z
+      * 4 bytes theta
+      * for each message {
+        2 bytes message length (n)
+        n bytes message
+      }
+      * 4 bytes (0000)
+
+      Returns:
+      -----------
+      b_string -> bytes
+        Bytes object representing entire packet
+      '''
+      b_string = struct.pack('=H4f', int(self.comm_id), float(
+        self.x), float(self.y), float(self.z), float(self.theta))
+      for msg in self.msgs:
+        b_string += struct.pack('H', len(msg))
+        b_string += msg
+      b_string += struct.pack('I', 0)
+      while len(b_string) < MSG_SIZE:
+        b_string += struct.pack('B', 0)
+      return b_string
+
+
+
     @staticmethod
-    def from_socket(s):
-        addr = ('0.0.0.0', 4242)
+    def from_socket(socketUDP):
+      '''
+      Create a packet from a socket
+      Block until a string of bytes comes in, and unpack these bytes into a new Packet object.
+      The incoming bytes are in the form described in the documentation for Packet.byte_string
+
+      Parameters:
+      ------------
+      s: socket object
+        Socket object
+
+      Returns:
+      ---------
+      Packet object ~ if successful
+
+      False         ~ if socket.error occured
+      '''
+      addr = ('0.0.0.0', 4242)
+      try:
         try:
-            try:
-                m, addr = s.recvfrom(MSG_SIZE)
-                # print(f"m={m} -=- addr={addr}")
-            except:
-                return False
-
-            if len(m) == 0:
-                # The socket is broken
-                return False
-
-            # Process the message
-            sender_id, x, y, z, theta = struct.unpack_from('=H4f', m)
-            # print('Pos ({},{},{}) angle {} of ID: {}'.format(x,y,z,theta,sender_id) )
-            tot = struct.calcsize('=H4f')
-            msgs = []
-            while (tot < MSG_SIZE):
-                try:
-                    msg_size = struct.unpack_from('H', m, tot)[0]
-                    tot += 2
-                except struct.error(e):
-                    print(e)
-                    return False
-                if msg_size == 0:
-                    break
-                msgs.append(m[tot:tot+msg_size])
-
-                tot += msg_size
-                print('rcv msg from {} size {} tot {}'.format(sender_id, msg_size, tot))
-            return Packet(x, y, z, sender_id, msgs, received_time=time.time(), addr=addr)
+          msg, addr = socketUDP.recvfrom(MSG_SIZE)
+          # print(f"m={m} -=- addr={addr}")
         except:
+          return False
+
+        if len(msg) == 0:
+          # The socket is broken
+          return False
+
+        # Process the message
+        sender_id, x, y, z, theta = struct.unpack_from('=H4f', msg)
+        # print('Pos ({},{},{}) angle {} of ID: {}'.format(x,y,z,theta,sender_id) )
+        tot = struct.calcsize('=H4f')
+        msgs = []
+        while (tot < MSG_SIZE):
+          try:
+            msg_size = struct.unpack_from('H', msg, tot)[0]
+            tot += 2
+          except struct.error as e:
+            print(e)
             return False
+          if msg_size == 0:
+            break
+          msgs.append(msg[tot:tot+msg_size])
+
+          tot += msg_size
+          print('rcv msg from {} size {} tot {}'.format(sender_id, msg_size, tot))
+        return Packet(x, y, z, sender_id, msgs, received_time=time.time(), addr=addr)
+      except:
+        return False
