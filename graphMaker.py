4a01853a40b75a650e4c0e6f3d02cc22ea9e556f
AidenNeale <aidenneale@sky.com>
2022-04-07 21:27:04 +0100

    Added functionality to retrieve total average swarm distance

--- graphMaker.py
+++ graphMaker.py
@@ -1,20 +1,48 @@
 from threading import Thread, Lock
+import itertools
+import math
 import matplotlib.pyplot as plt
-
+import time
+import numpy
 
 class graphMaker():
-  def __init__(self, commHub):
+  def __init__(self, commHub=None, frequency=1, experiment_length=30):
     self.commHub = commHub
+    self.frequency = frequency
+    self.experiment_length = experiment_length
 
-    self.X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    self.Y = [100, 120, 132, 140, 156, 158, 172, 189, 200, 225]
-
+    self.current_time = 0
+    self.X = []
+    self.Y = []
     self.gatherDataThread = Thread(target=self.gather_data, name="Retrieve Data")
     self.gatherDataThread.start()
 
 
   def gather_data(self):
-    print(self.commHub.get_locations())
+    while self.experiment_length > self.current_time:
+      saved_locations = self.commHub.get_locations()
+      total_dist = 0
+      num_of_indiv_comps = 0
+
+      try:
+        for comparison in itertools.combinations(saved_locations.values(), 2):
+          #Comparison saved in the form ((ID, POSE), (ID, POSE))
+          distance = math.sqrt(math.pow(comparison[0][0] - comparison[1][0], 2) +
+                              math.pow(comparison[0][1] - comparison[1][1], 2) +
+                              math.pow(comparison[0][2] - comparison[1][2], 2))
+          total_dist += distance
+          num_of_indiv_comps += 1
+
+        total_average_distance = total_dist / num_of_indiv_comps
+
+        self.X.append(self.current_time)
+        self.Y.append(total_average_distance)
+      except ZeroDivisionError:
+        continue # Robot Comm Hub not yet initialised
+      time.sleep(self.frequency)
+      self.current_time += self.frequency
+
 
   def draw_graphs(self):
-    pass
+    print(self.X)
+    print(self.Y)
