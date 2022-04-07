b239f64485ed3d544285c2215bd44f73d4f13b5e
AidenNeale <aidenneale@sky.com>
2022-04-07 16:39:11 +0100

    Added temporary commented graph code

--- CommHub_udp.py
+++ CommHub_udp.py
@@ -5,6 +5,7 @@ from threading import Lock
 
 from ArUcoTracker import ArUcoTracker
 from commhub import CommHub
+from graphMaker import graphMaker
 
 # Parameters for the Buzz ComHub
 FORWARD_FREQ = 500  # Hz
@@ -17,6 +18,17 @@ if __name__ == '__main__':
 
   comm_hub = CommHub(forward_freq=FORWARD_FREQ, host=SERVER_IP, port=PORT)
   robotTracker = ArUcoTracker(HOST=SERVER_IP, PORT=PORT, CommHub=comm_hub)
+
+  graphs = graphMaker()
+
+  # try:
+  #   while graphs.gatherDataThread.is_alive():
+  #     time.sleep(0.1)
+  #   graphs.draw_graphs()
+
+  # except KeyboardInterrupt:
+  #   pass
+
   try:
     while True:
       time.sleep(0.1)
