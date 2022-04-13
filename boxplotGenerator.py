a8e174091ae7fb0995d74d288c20870eda63cf56
AidenNeale <aidenneale@sky.com>
2022-04-13 17:51:03 +0100

    Added ability to plot both results

--- boxplotGenerator.py
+++ boxplotGenerator.py
@@ -4,9 +4,14 @@ import numpy
 
 if __name__ == '__main__':
   my_control_data = numpy.genfromtxt('results/control_data.csv', delimiter=',')
+
   packet_loss_data_25 = numpy.genfromtxt('results/packet_loss_25.csv', delimiter=',')
   packet_loss_data_50 = numpy.genfromtxt('results/packet_loss_50.csv', delimiter=',')
   packet_loss_data_75 = numpy.genfromtxt('results/packet_loss_75.csv', delimiter=',')
+
+  # position_noise_data_1cm = numpy.genfromtxt('results/position_noise_data_1cm.csv', delimiter=',')
+  # position_noise_data_5cm = numpy.genfromtxt('results/position_noise_data_5cm.csv', delimiter=',')
+  # position_noise_data_10cm = numpy.genfromtxt('results/position_noise_data_10cm.csv', delimiter=',')
 
 
   my_dict = {'Control': my_control_data,
@@ -15,12 +20,21 @@ if __name__ == '__main__':
               'Packet Loss 75%': packet_loss_data_75
             }
 
+  # my_dict = {'Control': my_control_data,
+  #             'Position Noise 1cm std': position_noise_data_1cm,
+  #             'Position Noise 5cm std': position_noise_data_5cm,
+  #             'Position Noise 10cm std': position_noise_data_10cm
+  #           }
+
   packet_loss_title = "Comparison showing Buzz's aggregation performance when experiencing packet loss"
   positioning_noise_title = "Comparison showing Buzz's aggregation performance when introducing positioning noise"
 
   plt.boxplot(my_dict.values())
   plt.title(packet_loss_title)
+  # plt.title(positioning_noise_title)
   plt.xlabel('Experiments')
   plt.ylabel('Average Distance (m) after 45 time units')
   plt.xticks([1, 2, 3, 4], my_dict.keys())
+  plt.savefig('graphs/packet_loss_boxplots.png')
   plt.show()
+  plt.clf()
